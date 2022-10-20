2717b2c33c30edb91f244566b2cf82dcb231c546
2051052029Duc <2051052029duc@ou.edu.vn>
2022-10-20 22:43:29 +0700

    third commit

--- SaleApp/dao.py
+++ SaleApp/dao.py
@@ -5,6 +5,8 @@ def load_categories():
     with open(f'{app.root_path}\Data\categories.json', encoding='utf-8') as f:
         return json.load(f)
 
+
+
 def load_products():
     with open(f'{app.root_path}\Data\products.json', encoding='utf-8') as f:
         return  json.load(f)
